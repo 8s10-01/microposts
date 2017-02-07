a09fa56f586814092c91cd015c3360dfbefb0578
Yasuhito Maruichi <maruichi-yasuhito@t3-lab.com>
2017-02-07 00:28:05 +0000

    ”7.6

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  # get 'users/new'
-
   root to: 'static_pages#home'
-  get 'signup', to: 'users#new'
-  
-  get 'login' , to: 'session#new'
-  get 'login' , to: 'session#create'
-  get 'logout' , to: 'session#destroy'
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   resources :users
 end
