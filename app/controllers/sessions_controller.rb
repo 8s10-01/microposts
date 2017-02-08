6e521bc1d8558e5743a7fd60358a77999ea13182
Yasuhito Maruichi <maruichi-yasuhito@t3-lab.com>
2017-02-08 06:09:28 +0000

    8.4 finish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,4 +18,11 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path
   end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -17,6 +18,23 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  # # # #
+  
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はプロファイルページへリダイレクト
+      redirect_to user_path , notice: '基本情報を編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  # # # #
 
   private
 
@@ -24,6 +42,9 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
-
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
   
 end
