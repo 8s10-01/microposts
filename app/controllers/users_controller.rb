eebae82fd69d80a2f6d4048dee8c12abc7a8ba39
Yasuhito Maruichi <maruichi-yasuhito@t3-lab.com>
2017-02-09 11:47:19 +0000

    (master) add followings/follwers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :check_session, only: [:edit, :update, :show]
-  before_action :set_user, only: [:edit, :update, :show]
+  before_action :check_session, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
