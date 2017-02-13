509af1d1692642aa038b12d220741a55a43a353a
Yasuhito Maruichi <maruichi-yasuhito@t3-lab.com>
2017-02-13 07:31:25 +0000

    ing-er Ver1.0

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,8 +1,14 @@
 class MicropostsController < ApplicationController
-    
+    # ApplicationControllerにあるlogged_in_userメソッドを実行し、
+    # ログインしていない場合はcreateメソッドは実行しないで
+    # /loginにリダイレクトするようにしています。
     before_action :logged_in_user, only: [:create]
     
     def create
+        # パラメータを受け取って現在のユーザーに紐付いたMicropostの
+        # インスタンスを作成して@micropost変数に入れ、
+        # @micropost.saveで保存が成功した場合は、root_urlである/にリダイレクトを行い、
+        # 失敗した場合はapp/views/static_pages/home.html.erbのテンプレートを表示します。
         @micropost = current_user.microposts.build(micropost_params)
         if @micropost.save
             flash[:success] = "Microposts created!"
@@ -13,6 +19,9 @@ class MicropostsController < ApplicationController
     end
     
     def destroy
+        # 投稿が現在のユーザーのものでなければ、root_urlにリダイレクトするようにしています。
+        # 投稿が現在のユーザーのものであれば、@micropost.destroy で削除し、
+        # リダイレクトを行っています。
         @micropost = current_user.microposts.find_by(id: params[:id])
         return redirect_to root_url if @micropost.nil?
         @micropost.destroy
@@ -21,6 +30,9 @@ class MicropostsController < ApplicationController
     end
     
     private
+    # フォームから受け取ったパラメータのparams[:micropost]のうち、
+    # params[:micropost][:content]のみデータの作成に使用するように
+    # Strong Parametersを宣言しています。
     def micropost_params
         params.require(:micropost) .permit(:content)
     end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
+  # ログインしている場合は、
+  # 新しいMicropostクラスのインスタンスをuser_idを紐付けた状態で初期化します。
+  # current_user.microposts.buildは、Micropost.new(user_id: current_user.id)と同じです。
+  # 前者の方はcurrent_userのhas_many :micropostsで生成されるbuildメソッドを使用していて、
+  # 確実にuser_idが紐付いたデータを作成できるのでこちらを使用するようにしましょう。
   def home
     @micropost = current_user.microposts.build if logged_in?
   end
